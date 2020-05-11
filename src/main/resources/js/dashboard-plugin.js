d23c440d861177a3c14b56bbf9c1016ff38b3c40
Valentin Bootz <valentinbootz@Valentins-MBP.fritz.box>
2020-05-11 14:40:16 +0200

    [PIT1-10] Item now accumulates finished tasks to display sorted ranking of developers

--- src/main/resources/js/dashboard-plugin.js
+++ src/main/resources/js/dashboard-plugin.js
@@ -1,4 +1,5 @@
 define('jira-dashboard-items/leaderboard', ['underscore', 'jquery', 'wrm/context-path'], function (_, $, contextPath) {
+
     var DashboardItem = function (API) {
         this.API = API;
         this.issues = [];
@@ -16,13 +17,35 @@ define('jira-dashboard-items/leaderboard', ['underscore', 'jquery', 'wrm/context
         this.requestData().done(function (data) {
             self.API.hideLoadingBar();
             self.issues = data.issues;
+
+            /**
+            * If response contains no issues use .Empty template
+            */
             if (self.issues === undefined || self.issues.length  === 0) {
                 $element.empty().html(Leaderboard.Dashboard.Item.Templates.Empty());
             }
+            /**
+            * Otherwise populate leaderboard array with assignees of issues in response
+            */
             else {
-                //TODO 
-
-                $element.empty().html(Leaderboard.Dashboard.Item.Templates.IssueList({issues: self.issues}));
+                self.leaderboard = [];
+                self.issues.forEach(issue => {
+                    var assignee = issue.fields.assignee;
+                    name = assignee ? assignee.name : "Unassigned"
+                    var index = self.leaderboard.findIndex(element => element.name == name);
+                    if (index == -1) {
+                        try {
+                            avatar = assignee.avatarUrls["16x16"];
+                        } catch (error) {
+                            avatar = "/jira/secure/useravatar?size=xsmall&avatarId=10123";
+                        }
+                        self.leaderboard.push(new Developer(avatar, name, 1));
+                    } else {
+                        self.leaderboard[index].issuecount++;
+                    }
+                });
+                self.leaderboard.sort((a,b) => (a.issuecount > b.issuecount) ? -1 : ((b.issuecount > a.issuecount) ? 1 : 0))
+                $element.empty().html(Leaderboard.Dashboard.Item.Templates.Leaderboard({leaderboard: self.leaderboard}));
             }
             self.API.resize();
             $element.find(".submit").click(function (event) {
@@ -34,12 +57,26 @@ define('jira-dashboard-items/leaderboard', ['underscore', 'jquery', 'wrm/context
         this.API.once("afterRender", this.API.resize);
     };
 
+    /**
+    * REST call requesting all issues with status 'Done'
+    */
     DashboardItem.prototype.requestData = function () {
         return $.ajax({
             method: "GET",
             url: contextPath() + "/rest/api/latest/search?jql=status%3Ddone"
         });
     };
+
+    /**
+    * Developer class for leaderbaord items
+    */
+    class Developer {
+        constructor(avatar, name, issuecount) {
+          this.avatar = avatar;
+          this.name = name;
+          this.issuecount = issuecount;
+        };
+      };
 
     return DashboardItem;
 });
