c8d001b497e411442183feb572d6c9f08f6ad26e
Katharina Stetter <>
2020-05-25 18:20:34 +0200

    The Item works and gives an table of users whose tasks have the search parameter in their title

--- src/main/resources/js/expert-dashboard-item.js
+++ src/main/resources/js/expert-dashboard-item.js
@@ -4,64 +4,65 @@ define('jira-dashboard-items/expert', ['underscore', 'jquery', 'wrm/context-path
         this.API = API;
         this.issues = [];
     };
+    
     /**
     * Called to render the view for a fully configured dashboard item.
-    *
-    * @param context The surrounding <div/> context that this item should render into.
-    * @param preferences The user preferences saved for this dashboard item (e.g. filter id, number of results...)
     */
     DashboardItem.prototype.render = function (context, preferences) {
         this.API.showLoadingBar();
         var $element = this.$element = $(context);
         var self = this;
-        var expertTable = [];
-        var expertTableNumber = [];
         var keyword = "";
         
+        //Event to get our keyword
         $(context).on('change','#search-text', function(){
             keyword = this.value;
         })
+
+        
+        //request issues that have been done and create the espert table
         this.requestData().done(function (data) {
             self.API.hideLoadingBar();
             self.issues = data.issues;
             
 
             if(self.issues === undefined || self.issues.length  === 0){
-                //do nothing for now
+
             }
+            
+            //only use expert search if there are actually done items
             else{
+                
                 $(context).on('click', '#search', function(){
+                    var expert = [];
                     
-                    //filter issues
+                    //filter issues that have keyword in their title
                     var expertIssues = filterIssuesWithKeyword(self.issues, keyword);
                     
-                    //create array for table with names and nummbers of keyword issues
-                    if(expertIssues === undefined || expertIssues.length  === 0){
-                    }
-                    else{
-                        for(ei = 0; ei < expertIssues.length; ei++){
-                            if(expertIssues[ei].changelog === undefined){
-                            }else{
-                                var developer = getDeveloper(expertIssues[ei].changelog);
-                                if(developer === undefined){
-                                }else{
-                                    var index = expertTable.indexOf(developer);
-                                    if(expertTable.length === 0 || expertTable === undefined ){
-                                        expertTable.push(developer);
-                                        expertTableNumber.push(1);
-                                    }else{
-                                        expertTableNumber[index]++;
-                                    }
-                                }
+                    //map experts and the number of filtered issues they worked on
+                    expertIssues.forEach(issue => {
+                        var developer = getDeveloper(issue.changelog);
+                        name = developer ? developer.name : "Unspecified"
+                        var index = expert.findIndex(element => element.name == name);
+                        
+                        if (index == -1) {
+                            try {
+                                avatar = developer.avatarUrls["16x16"];
+                            } catch (error) {
+                                avatar = "/jira/secure/useravatar?size=xsmall&avatarId=10123";
                             }
+                            expert.push(new User({avatar, name, issues: 1}));
+                            
+                        } else {
+                            expert[index].issues++;
                         }
-                    }
+                    });
                     
-                    //create the table so we can give it out in html
-                    var bool = $(context).find('#test-table').innerHTML = "Hello";
-                    
-                    var x = "test";
+                    //order entrences by issue count
+                    expert.sort((a,b) => (a.issues > b.issues) ? -1 : ((b.issues > a.issues) ? 1 : 0))
+                    $(context).find('#expert-table').html(createExpertTable(expert));
                 });
+                
             }
         });
 
@@ -84,42 +85,42 @@ define('jira-dashboard-items/expert', ['underscore', 'jquery', 'wrm/context-path
     }
     
     /**
-    * Gets developer from issue changelog. Credited is author who last changed status to 'In Progress'
-    *
-    * @param changelog
+    Get the developer of an issue, aka the last user that set the item to "In Progress"
     */
     function getDeveloper(changelog) {
-        var developers = [];
         
-        for(i = 0; i < changelog.histories.length; i++){
-            if(changelog.histories[i].items[0].toString === "In Progress"){
-                developers.push(changelog.histories[i].author.name);
-            }
-        }
-        return developers[developers.length-1];
+        entry = changelog.histories.filter(
+            function(histories){ return histories.items[0].toString == 'In Progress' }
+        ).slice(-1)[0];
+        return entry ? entry.author : undefined;
     }
+    
+    /**
+     Class to greate a user object that is later used for the expert table
+     */
+    class User {
+        constructor({avatar, name, issues} = {}) {
+            this.avatar = avatar;
+            this.name = name;
+            this.issues = issues;
+        };
+    };
     
     /**
      Creates a table with the names of the experts and the nummber of issues they solved.
      */
-    function createExpertTable(expertName, expertNummber){
-        if(expertNummber === undefined  || expertName === undefined){
-            return "No Experts found";
-        }
-        else{
-            //var sortedNumb = expertNummber;
-             //sortedNumb.sort();
-             var tabletext = "<table class=" + "aui" + "><thead><tr><th id=" + "name" + ">Name</th><th id=" + "number" + ">Ranking</th></tr></thead><tbody>";
-             
-            for(ti = 0; ti < expertNummber.length; ti++){
-                 tabletext = tabletext + "<tr><td headers=" + "name" +">" + expertNummber[ti] +"</td><td headers="+ "number" +">" + expertName[ti] + "</td></tr>";
-             }
-             return tabletext + "</tbody></table>";
-        }
+    function createExpertTable(experts){
+        var table = "<thead><tr><th id=" + "name"+ ">User</th><th id=" +"issues"+ ">Completed Issues</th></tr></thead><tbody>";
+        
+        experts.forEach(expert => {
+            table = table + "<tr><td headers=" + "name" + "><span class=" + "container" + "><span class=" + "aui-avatar aui-avatar-xsmall" + "><span class=" + "aui-avatar-inner" + "><img src= " +expert.avatar +  "alt=" + " role=" + "presentation"+ "/></span></span>" + expert.name + "</span></td><td headers=" + "issues" + ">" + expert.issues + "</td></tr>"
+        });
+        
+        return table + "</tbody>";
     }
 
     /**
-    * REST call requesting all issues with status 'Done' with expanded changelog
+    REST call requesting all issues with status 'Done' with expanded changelog
     */
     DashboardItem.prototype.requestData = function () {
         return $.ajax({
